e0dfae2730a717cbcb7727bafad276a8e474737e
Mike Mattingly <cmattingly1584@gmail.com>
2021-06-24 15:51:43 -0700

    fixes bugs and final mobile viewport layout

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,21 +3,30 @@ const Movie = require('../models/movie');
 
 module.exports = {
     create,
+    delete: deleteComment
 };
 
 
 function create(req, res) {
-    console.log(req.params.id);
     Movie.findById(req.params.id, function(err, movie) {
       req.body.user = req.user._id;
       req.body.userName = req.user.name;
-      console.log(req.body.user, "req.body.user");
-      console.log(req.body.userName, "req.body.userName");
-      console.log(movie.comments, "movie.comments before");
       movie.comments.push(req.body);
-      console.log(movie.comments, "movie.comments after");
       movie.save(function(err) {
         res.redirect(`/movies/${movie._id}`);
       });
     });
+  }
+
+  function deleteComment(req, res) {
+    Movie.findOne(
+      {'comments._id': req.params.id}, function(err, movie) {
+        if (!movie || err) return res.redirect(`/movies/${movie._id}`);
+        const commentSubdoc = movie.comments.id(req.params.id)
+        commentSubdoc.remove();
+        movie.save(function(err) {
+          res.redirect(`/movies/${movie._id}`);
+        });
+      }
+    );
   }
